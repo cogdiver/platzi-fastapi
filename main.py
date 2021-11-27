80476f65a3fbf3066d35282cc0320760d7b4ef65
Valentina Arenas <valentina.lozano@jikkosoft.com>
2021-11-27 10:58:19 -0500

    post_put_delete_classes

--- main.py
+++ main.py
@@ -948,7 +948,6 @@ def post_course(course: CourseInfoBasic =  Body(...)):
     
     return course
 
-
 @app.put(
     path="/cursos/{id_course}",
     response_model=CourseInfoBasic,
@@ -1093,6 +1092,13 @@ def delete_course(id_course):
     tags=["Class"]
 )
 def all_classes():
+    """
+    This path operation returns all classes
+
+    Parameters:
+
+    Returns a list of classes with a BaseClass structure:
+    """
     with open('./data/classes.json') as f:
         classes = json.loads(f.read())
     
@@ -1106,6 +1112,14 @@ def all_classes():
     tags=["Class"]
 )
 def get_classes_basic(id_class):
+    """
+    This path operation return the basic description for a class
+
+    Parameters:
+        - id_class: str
+    
+    Returns a class with with a ClassContentBasic structure:
+    """
     with open('./data/classes.json') as f:
         classes = json.loads(f.read())
 
@@ -1134,7 +1148,15 @@ def get_classes_basic(id_class):
     summary="get a complete description of a class",
     tags=["Class"]
 )
-def get_classes(id_course, id_class):
+def get_class(id_course, id_class):
+    """
+    This path operation return the complete description for a class
+
+    Parameters:
+        - id_class: str
+    
+    Returns a class with with a ClassContent structure:
+    """
     with open('./data/courses.json') as f:
         courses = json.loads(f.read())
     
@@ -1249,43 +1271,194 @@ def get_classes(id_course, id_class):
     return class_
 
 @app.post(
-    path="/clases/{id_class}",
+    path="/clases",
     response_model=ClassContentBasic,
     status_code=status.HTTP_201_CREATED,
     summary="create a class for a course",
     tags=["Class"]
 )
-def post_classes():
-    with open('./data/classes.json') as f:
-        classes = json.loads(f.read())
+def post_classes(class_: ClassContentBasic = Body(...)):
+    """
+    This path operation create a new class
+
+    Parameters:
+        - class_: ClassContentBasic
     
-    return classes
+    Return the new class in a json with a ClassContentBasic structure
+    """
+    class_ = class_.dict()
+    with open('./data/classes.json', 'r', encoding='utf-8') as f:
+        classes = json.loads(f.read())
+
+    # id_class must be unique
+    id_classes = list(map(lambda c: c['id_class'], classes))
+    if class_["id_class"] in id_classes:
+        raise HTTPException(
+            status_code=406,
+            detail=f"HTTP_406_NOT_ACCEPTABLE: Invalid id class '{class_['id_class']}'"
+        )
+    del id_classes
+
+    # name must be unique
+    name_classes = list(map(lambda c: c['name'], classes))
+    if class_["name"] in name_classes:
+        raise HTTPException(
+            status_code=406,
+            detail=f"HTTP_406_NOT_ACCEPTABLE: Invalid name class '{class_['name']}'"
+        )
+    del name_classes
+
+    # id_comments must not be in class_
+    if class_["id_comments"]:
+        print(class_)
+        raise HTTPException(
+            status_code=406,
+            detail=f"HTTP_406_NOT_ACCEPTABLE: Invalid key 'id_comments'"
+        )
+
+    # Parsing class resourses
+    class_["video_url"] = str(class_["video_url"])
+    for r in class_["resourses"]:
+        r["url"] = str(r["url"])
+    
+    # Save the class_
+    classes.append(class_)
+    with open('./data/classes.json', 'w', encoding='utf-8') as f:
+        f.write(json.dumps(classes, ensure_ascii=False))
+    
+    return class_
 
 @app.put(
-    path="/clases/{id_course}/{id_class}",
+    path="/clases/{id_class}",
     response_model=ClassContentBasic,
     status_code=status.HTTP_200_OK,
     summary="update a class",
     tags=["Class"]
 )
-def put_classes():
-    with open('./data/classes.json') as f:
+def put_classes(id_class, class_: ClassContentBasic = Body(...)):
+    """
+    This path operation update a new class
+
+    Parameters:
+        - class_: ClassContentBasic
+    
+    Return the updated class in a json with a ClassContentBasic structure
+    """
+    class_ = class_.dict()
+    with open('./data/classes.json', 'r', encoding='utf-8') as f:
         classes = json.loads(f.read())
     
-    return classes
+    # id_class must be valid
+    id_classes = list(map(lambda c: c['id_class'], classes))
+    if id_class not in id_classes:
+        raise HTTPException(
+            status_code=406,
+            detail=f"HTTP_406_NOT_ACCEPTABLE: Invalid id class '{id_class}'"
+        )
+    
+    classes = list(filter(lambda c: c["id_class"] != id_class, classes))
+
+    # id_class must be unique
+    if class_["id_class"] in id_classes:
+        raise HTTPException(
+            status_code=406,
+            detail=f"HTTP_406_NOT_ACCEPTABLE: Invalid id class '{class_['id_class']}'"
+        )
+    del id_classes
+
+    # name must be unique
+    name_classes = list(map(lambda c: c['name'], classes))
+    if class_["name"] in name_classes:
+        raise HTTPException(
+            status_code=406,
+            detail=f"HTTP_406_NOT_ACCEPTABLE: Invalid name class '{class_['name']}'"
+        )
+    del name_classes
+
+    # Parsing id_comments
+    for i in range(len(class_["id_comments"])):
+        class_["id_comments"][i] = str(class_["id_comments"][i])
+    
+    # id_comments must be valid
+    with open('./data/comments.json', 'r', encoding='utf-8') as f:
+        comments = json.loads(f.read())
+    
+    id_comments = list(map(lambda c: c["id_contribution"], comments))
+    del comments
+
+    for c in class_["id_comments"]:
+        if c not in id_comments:
+            raise HTTPException(
+                status_code=406,
+                detail=f"HTTP_406_NOT_ACCEPTABLE: Invalid id comment '{c}'"
+            )
+
+    # Parsing class resourses
+    class_["video_url"] = str(class_["video_url"])
+    for r in class_["resourses"]:
+        r["url"] = str(r["url"])
+    
+    # Save the class_
+    classes.append(class_)
+    with open('./data/classes.json', 'w', encoding='utf-8') as f:
+        f.write(json.dumps(classes, ensure_ascii=False))
+    
+    return class_
 
 @app.delete(
-    path="/clases/{id_course}/{id_class}",
+    path="/clases/{id_class}",
     response_model=ClassContentBasic,
     status_code=status.HTTP_200_OK,
-    summary="delete a class from a course",
+    summary="delete a class",
     tags=["Class"]
 )
-def delete_classes():
-    with open('./data/classes.json') as f:
-        classes = json.loads(f.read())
+def delete_classes(id_class):
+    """
+    This path operation delete a class
+
+    Parameters:
+        - id_class: str
     
-    return classes
+    Return the deleted class in a json with a ClassContentBasic structure
+    """
+    with open('./data/classes.json', 'r', encoding='utf-8') as f:
+        classes = json.loads(f.read())
+
+    # id_class must be valid
+    id_classes = list(map(lambda c: c['id_class'], classes))
+    if id_class not in id_classes:
+        raise HTTPException(
+            status_code=406,
+            detail=f"HTTP_406_NOT_ACCEPTABLE: Invalid id class '{id_class}'"
+        )
+        
+    # delete class from courses
+    with open('./data/courses.json', 'r', encoding='utf-8') as f:
+        courses = json.loads(f.read())
+    
+    courses = list(
+        map(
+            lambda c: {**c, **{"modules": list(
+                map(
+                    lambda m: {**m, **{"id_classes": [c for c in m["id_classes"] if c != id_class]}},
+                    c["modules"]
+                )
+            )}},
+            courses
+        )
+    )
+
+    with open('./data/courses.json', 'w', encoding='utf-8') as f:
+        f.write(json.dumps(courses, ensure_ascii=False))
+    del courses
+
+    # Save the class_
+    class_ = list(filter(lambda  c: c["id_class"] == id_class, classes))[0]
+    classes = list(filter(lambda  c: c["id_class"] != id_class, classes))
+    with open('./data/classes.json', 'w', encoding='utf-8') as f:
+        f.write(json.dumps(classes, ensure_ascii=False))
+    
+    return class_
 
 
 # Contributions
