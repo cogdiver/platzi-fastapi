4229e1da9da9361c79c11c03bdd9c46ef40fbfe0
Valentina Arenas <valentina.lozano@jikkosoft.com>
2021-12-27 08:55:44 -0500

    Deploy

--- main.py
+++ main.py
@@ -2,24 +2,24 @@
 from fastapi import FastAPI
 
 # Routes
-from routes.categories import categories_routes
-from routes.routes import routes_routes
-from routes.courses import courses_routes
-from routes.classes import classes_routes
-from routes.comments import comments_routes
-from routes.blogs import blogs_routes
-from routes.forums import forums_routes
-from routes.tutorials import tutorials_routes
+# from routes.categories import categories_routes
+# from routes.routes import routes_routes
+# from routes.courses import courses_routes
+# from routes.classes import classes_routes
+# from routes.comments import comments_routes
+# from routes.blogs import blogs_routes
+# from routes.forums import forums_routes
+# from routes.tutorials import tutorials_routes
 
 app = FastAPI()
-app.include_router(categories_routes, prefix='/categoria')
-app.include_router(routes_routes, prefix='/rutas')
-app.include_router(courses_routes, prefix='/cursos')
-app.include_router(classes_routes, prefix='/clases')
-app.include_router(comments_routes, prefix='/comentarios')
-app.include_router(blogs_routes, prefix='/blogs')
-app.include_router(forums_routes, prefix='/foros')
-app.include_router(tutorials_routes, prefix='/tutoriales')
+# app.include_router(categories_routes, prefix='/categoria')
+# app.include_router(routes_routes, prefix='/rutas')
+# app.include_router(courses_routes, prefix='/cursos')
+# app.include_router(classes_routes, prefix='/clases')
+# app.include_router(comments_routes, prefix='/comentarios')
+# app.include_router(blogs_routes, prefix='/blogs')
+# app.include_router(forums_routes, prefix='/foros')
+# app.include_router(tutorials_routes, prefix='/tutoriales')
 
 
 # Home
