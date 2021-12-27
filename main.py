01e2d19912cc867e1c7c7e559ac0af17d7258a40
Valentina Arenas <valentina.lozano@jikkosoft.com>
2021-12-27 09:24:49 -0500

    cors

--- main.py
+++ main.py
@@ -1,25 +1,38 @@
 # FastAPI
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 # Routes
-# from routes.categories import categories_routes
-# from routes.routes import routes_routes
-# from routes.courses import courses_routes
-# from routes.classes import classes_routes
-# from routes.comments import comments_routes
-# from routes.blogs import blogs_routes
-# from routes.forums import forums_routes
-# from routes.tutorials import tutorials_routes
+from routes.categories import categories_routes
+from routes.routes import routes_routes
+from routes.courses import courses_routes
+from routes.classes import classes_routes
+from routes.comments import comments_routes
+from routes.blogs import blogs_routes
+from routes.forums import forums_routes
+from routes.tutorials import tutorials_routes
 
 app = FastAPI()
-# app.include_router(categories_routes, prefix='/categoria')
-# app.include_router(routes_routes, prefix='/rutas')
-# app.include_router(courses_routes, prefix='/cursos')
-# app.include_router(classes_routes, prefix='/clases')
-# app.include_router(comments_routes, prefix='/comentarios')
-# app.include_router(blogs_routes, prefix='/blogs')
-# app.include_router(forums_routes, prefix='/foros')
-# app.include_router(tutorials_routes, prefix='/tutoriales')
+
+origins = [
+    "http://localhost/*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+app.include_router(categories_routes, prefix='/categoria')
+app.include_router(routes_routes, prefix='/rutas')
+app.include_router(courses_routes, prefix='/cursos')
+app.include_router(classes_routes, prefix='/clases')
+app.include_router(comments_routes, prefix='/comentarios')
+app.include_router(blogs_routes, prefix='/blogs')
+app.include_router(forums_routes, prefix='/foros')
+app.include_router(tutorials_routes, prefix='/tutoriales')
 
 
 # Home
